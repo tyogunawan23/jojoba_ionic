94da87c0dbf5fbbf26b0cd219c992056c042c027
tyo <tyo@code.id>
2016-08-18 01:46:41 +0700

    no message

--- www/views/chat/chat.js
+++ www/views/chat/chat.js
@@ -9,12 +9,3 @@ controllerModule.controller("chat", function($scope, Chats){
 controllerModule.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 });
-
-
-controllerModule.controller('CardCtrl', function($scope, $ionicSwipeCardDelegate) {
-  $scope.doAnything = function() {
-    var card = $ionicSwipeCardDelegate.getSwipeableCard($scope);
-    card.swipe();
-    //  alert("tes");
-  };
-})
